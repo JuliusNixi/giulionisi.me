13b0192a7ce091384b79b196b0d3516cac151706
Giulio Nisi <email@giulionisi.it>
2024-11-19 14:59:14 +0100

    .gitignore update

--- dev.sh
+++ dev.sh
@@ -1,4 +1,3 @@
-# Run with . ./dev.sh
 rm nohup.out
 nohup sh -c "tail -f /dev/null | npx tailwindcss -i ./src/styles/tailwindin.css -o ./src/styles/tailwindout.css --watch" &
 nohup npm run dev < /dev/null &
